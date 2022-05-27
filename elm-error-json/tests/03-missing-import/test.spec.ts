4ebf1ea80387383189a712e47bcb2f4fea0569e0
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2022-05-26 21:32:55 -0500

    update test name

--- elm-error-json/tests/03-missing-import/test.spec.ts
+++ elm-error-json/tests/03-missing-import/test.spec.ts
@@ -11,7 +11,7 @@ beforeAll(async () => {
   parsedError = await ElmErrorJson.compile(filepath)  
 })
 
-describe('02-typo', () => {
+describe('03-missing-import', () => {
   test("raw error is what we expect", () => {
     expect(rawJson).toBe("{\"type\":\"compile-errors\",\"errors\":[{\"path\":\"/home/ryan/code/elm-land/elm-land/elm-error-json/tests/03-missing-import/src/Main.elm\",\"name\":\"Main\",\"problems\":[{\"title\":\"NAMING ERROR\",\"region\":{\"start\":{\"line\":5,\"column\":5},\"end\":{\"line\":5,\"column\":14}},\"message\":[\"I cannot find a `Html.text` variable:\\n\\n5|     Html.text \\\"Hello, world!\\\"\\n       \",{\"bold\":false,\"underline\":false,\"color\":\"RED\",\"string\":\"^^^^^^^^^\"},\"\\nI cannot find a `Html` import. These names seem close though:\\n\\n    \",{\"bold\":false,\"underline\":false,\"color\":\"yellow\",\"string\":\"Cmd.map\"},\"\\n    \",{\"bold\":false,\"underline\":false,\"color\":\"yellow\",\"string\":\"Cmd.none\"},\"\\n    \",{\"bold\":false,\"underline\":false,\"color\":\"yellow\",\"string\":\"List.head\"},\"\\n    \",{\"bold\":false,\"underline\":false,\"color\":\"yellow\",\"string\":\"List.sort\"},\"\\n\\n\",{\"bold\":false,\"underline\":true,\"color\":null,\"string\":\"Hint\"},\": Read <https://elm-lang.org/0.19.1/imports> to see how `import`\\ndeclarations work in Elm.\"]}]}]}")
   })
