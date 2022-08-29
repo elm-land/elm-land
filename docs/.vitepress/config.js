f2fd84ad70936f5d7e4e0e2bbea53921b3560bca
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2022-08-28 20:49:16 -0500

    bump vitepress version

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -7,7 +7,6 @@ export default {
     ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Fira+Code:wght@500&family=Lora:wght@600&family=Nunito+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap' }],
     ['link', { rel: 'icon', href: '/images/logo-256.png' }],
   ],
-  ignoreDeadLinks: true,
 
   markdown: {
     lineNumbers: true,
