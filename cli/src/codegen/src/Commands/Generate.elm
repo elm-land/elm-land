bf95bfd37e5264ae6b4fe88ee35a8ee6889f4cfa
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2022-10-13 00:02:33 -0500

    update name in generated code

--- cli/src/codegen/src/Commands/Generate.elm
+++ cli/src/codegen/src/Commands/Generate.elm
@@ -594,6 +594,14 @@ runWhenAuthenticatedDeclaration =
                               , arguments = [ CodeGen.Argument.new "user" ]
                               , expression = CodeGen.Expression.value "toTuple user"
                               }
+                            , { name = "Auth.Action.ShowLoadingPage"
+                              , arguments = [ CodeGen.Argument.new "loadingView" ]
+                              , expression =
+                                    CodeGen.Expression.multilineTuple
+                                        [ CodeGen.Expression.value "Loading loadingView"
+                                        , CodeGen.Expression.value "Cmd.none"
+                                        ]
+                              }
                             , { name = "Auth.Action.ReplaceRoute"
                               , arguments = [ CodeGen.Argument.new "options" ]
                               , expression =
@@ -610,20 +618,12 @@ runWhenAuthenticatedDeclaration =
                                         , CodeGen.Expression.value "toCmd (Effect.pushRoute options)"
                                         ]
                               }
-                            , { name = "Auth.Action.ExternalRedirect"
+                            , { name = "Auth.Action.LoadExternalUrl"
                               , arguments = [ CodeGen.Argument.new "url" ]
                               , expression =
                                     CodeGen.Expression.multilineTuple
                                         [ CodeGen.Expression.value "Redirecting"
                                         , CodeGen.Expression.value "toCmd (Effect.loadExternalUrl url)"
-                                        ]
-                              }
-                            , { name = "Auth.Action.ShowLoadingPage"
-                              , arguments = [ CodeGen.Argument.new "loadingView" ]
-                              , expression =
-                                    CodeGen.Expression.multilineTuple
-                                        [ CodeGen.Expression.value "Loading loadingView"
-                                        , CodeGen.Expression.value "Cmd.none"
                                         ]
                               }
                             ]
